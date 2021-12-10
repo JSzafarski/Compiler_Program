ebec358860de6ed0de39043bbfee18206b3e0d2c
JSzafarski <92590150+JSzafarski@users.noreply.github.com>
2021-12-10 16:18:10 +0000

    pretty much completed

--- high level language.hs
+++ high level language.hs
@@ -28,19 +28,18 @@ data BExp =  Not Bc
 -- aval (Plus (N 3) (V "x")) (fromList [("x" ,0)])
 
 aval :: AExp -> State -> Val
-aval (Plus a b) state --i ahve to do a aval function for every combination
-                     | a == N c && b == V d = if isNothing(Machine.grabState d state) then -1 else
-                                    let f _ = Just((fromJust(Machine.grabState d l) + c))--fucntion that returns v
-                                    in (alter f d state)    --nope as we want to return the value not alter the states u mis-read
-                     | a == V c && b == N d = if isNothing(Machine.grabState c state) then -1 else
-                                    let f _ = Just((fromJust(Machine.grabState C state) + d))--fucntion that returns v
-                                    in (alter f c l)
-                     | a == V c && b == V d = if isNothing(Machine.grabState c state ) || isNothing(Machine.grabState d state )  then -1 else
-                                    fromJust(Machine.grabState (c state)) + fromJust(Machine.grabState (d state))
-                     | (a == (N c)) && (b == (N d)) = c + d               
-                     |otherwise = -1 --change this later idk
+aval (Plus a b) state = case a of 
+                        (N c) -> case b of
+                                (V d) -> if isNothing(Machine.grabState d state) then -1 else
+                                         fromJust(Machine.grabState d state) + c
+                                (N d) -> c + d
+                        (V c) -> case b of
+                                (V d) -> if isNothing(Machine.grabState c state ) || isNothing(Machine.grabState d state)  then -1 else
+                                         fromJust(Machine.grabState c state) + fromJust(Machine.grabState d state)  
+                                (N d) -> if isNothing(Machine.grabState c state) then -1 else
+                                         fromJust(Machine.grabState c state) + d 
 aval (N a) _ = a 
-aval (V a) state = if isNothing(Machine.grabState a state) then -1 else fromJust(Machine.grabState a state)  
+aval (V a) state = fromMaybe (- 1) (Machine.grabState a state)  
                          
 
 bval :: BExp -> State -> Bool
@@ -64,23 +63,29 @@ bval (Not a) state
 
 eval :: Com -> State -> State
 
-eval (Assign a (N b)) state = if isNothing(Machine.grabState a state) then state --make it add a variable if it doesn't exist?
-                              else let f _ = Just b
-                                   in alter f a state
+eval (Assign a b) state = case b of
+                             (N c) ->   if isNothing(Machine.grabState a state) then state --make it add a variable if it doesn't exist?
+                                        else let f _ = Just c
+                                             in alter f a state
+                             (V d) ->  if isNothing(Machine.grabState a state) then state --make it add a variable if it doesn't exist?
+                                       else let f _ = Machine.grabState d state
+                                            in alter f a state                    
+                             (Plus e g) -> let f _ = Just(aval (Plus e g) state)
+                                           in  alter f a state                   
 eval SKIP state = state                                   
  
 eval (Seq a b) stack =  let stack2 = case a of
                                 (Assign b (N c)) -> eval (Assign b (N c)) stack  
                                 (If d e f) -> eval (If d e f) stack
                                 (Seq g h) -> eval (Seq g h) stack  
-                                --(While i j) -> eval (While i j) stack  
+                                (While i j) -> eval (While i j) stack  
                                 SKIP -> eval SKIP stack  
                         in
                             case b of
                                 (Assign b (N c)) -> eval (Assign b (N c)) stack2  --passed for evaluaytion Rhs first
                                 (If d e f) -> eval (If d e f) stack2
                                 (Seq g h) -> eval (Seq g h) stack2  
-                                --(While i j) -> eval (While i j) stack2         
+                                (While i j) -> eval (While i j) stack2         
                                 SKIP -> eval SKIP stack2  
                   
 eval (If a b c) state =  case a of
@@ -88,41 +93,51 @@ eval (If a b c) state =  case a of
                                                                 (Assign b (N c)) -> eval (Assign b (N c)) state  --passed for evaluaytion Rhs first
                                                                 (If d e f) -> eval (If d e f) state
                                                                 (Seq g h) -> eval (Seq g h) state  
-                                                                --(While i j) -> eval (While i j) stack2         
+                                                                (While i j) -> eval (While i j) state         
                                                                 SKIP -> eval SKIP state
                                                                 else case c of
                                                                 (Assign b (N c)) -> eval (Assign b (N c)) state  --passed for evaluaytion Rhs first
                                                                 (If d e f) -> eval (If d e f) state
                                                                 (Seq g h) -> eval (Seq g h) state 
-                                                                --(While i j) -> eval (While i j) stack2         
+                                                                (While i j) -> eval (While i j) state        
                                                                 SKIP -> eval SKIP state      
                         (And g h) -> if bval (And g h) state then case b of
                                                                 (Assign b (N c)) -> eval (Assign b (N c)) state  --passed for evaluaytion Rhs first
                                                                 (If d e f) -> eval (If d e f) state
                                                                 (Seq g h) -> eval (Seq g h) state  
-                                                                --(While i j) -> eval (While i j) stack2         
+                                                                (While i j) -> eval (While i j) state         
                                                                 SKIP -> eval SKIP state
                                                                 else case c of
                                                                 (Assign b (N c)) -> eval (Assign b (N c)) state  --passed for evaluaytion Rhs first
                                                                 (If d e f) -> eval (If d e f) state
                                                                 (Seq g h) -> eval (Seq g h) state 
-                                                                --(While i j) -> eval (While i j) stack2         
+                                                                (While i j) -> eval (While i j) state         
                                                                 SKIP -> eval SKIP state     
 eval (While c d) state = case c of 
                           --(Not b) -> if b == TRUE then eval (While c d) -- will never chnage
-                          (Less e f) -> if BExp (Less e f) stack case d of
-                                (Assign b (N c)) -> eval while( ) (Assign b (N c)) state  --passed for evaluaytion Rhs first
-                                (If d e f) -> eval (If d e f) state
-                                (Seq g h) -> eval (Seq g h) state 
-                                --(While i j) -> eval (While i j) stack2         
-                                SKIP -> eval SKIP state 
-                                  While (Bexp (Less c d) )
-                          (And g h) ->  case d of
-                                (Assign b (N c)) -> eval (Assign b (N c)) state  --passed for evaluaytion Rhs first
-                                (If d e f) -> eval (If d e f) state
-                                (Seq g h) -> eval (Seq g h) state 
-                                --(While i j) -> eval (While i j) stack2         
-                                SKIP -> eval SKIP state 
-                                  While (Bexp (Less c d) )
-
+                          (Less e f) -> if bval(Less e f) state then case d of
+                                                                (Assign g q) -> let state2 = eval (Assign g q) state
+                                                                                in  eval (While c d) state2  --passed for evaluaytion Rhs first
+                                                                (If i j k) -> let state2 = eval (If i j k) state 
+                                                                        in  eval (While c d) state2
+                                                                (Seq l m)  -> let state2 = eval (Seq l m) state
+                                                                        in  eval (While c d) state2
+                                                                (While o p) -> let state2 = eval (While o p) state
+                                                                        in eval (While c d) state2         
+                                                                SKIP -> let state2 = eval SKIP state --will never terminate
+                                                                        in eval (While c d) state2
+                                        else state 
+                          (And g h) ->  if bval(And g h) state then case d of
+                                        (Assign i q) -> let state2 = eval (Assign i q) state
+                                                            in  eval (While c d) state2  --passed for evaluaytion Rhs first
+                                        (If k l m) -> let state2 = eval (If k l m) state 
+                                                      in  eval (While c d) state2
+                                        (Seq n o)  -> let state2 = eval (Seq n o) state
+                                                      in  eval (While c d) state2
+                                        (While p q) -> let state2 = eval (While p q) state
+                                                       in eval (While c d) state2         
+                                        SKIP -> let state2 = eval SKIP state --will never terminate
+                                                in eval (While c d) state2
+                                        else state         
+ 
 
